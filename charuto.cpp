3ba83ef6974bceab99b53cd22c77827f9eb4447c
Luciano Daniel <42324730+lodaniel@users.noreply.github.com>
2022-05-28 09:02:49 -0300

    Update charuto.cpp

--- charuto.cpp
+++ charuto.cpp
@@ -1,9 +1,10 @@
 /* Titulo: Um charuto
 /* Autor: Prof. Luciano Daniel
+/* */
 /* "aas vezes um charuto era apenas: um charuto" */
 /* "um charuto, aas vezes, apenas eh um charuto" */
 /* "as vezes que o charuto eh apenas ele mesmo" */
-/* "apenas um charuto aas vezes ele é" */
+/* "apenas um charuto aas vezes ele eh" */
 /* "de repente um charuto eh si mesmo" */
 /* "no interior de um charuto pode morar apenas seu eu" */
 /* "um charuto eh sua propria imagem, mas aas vezes apenas!" */
