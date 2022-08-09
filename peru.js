a2638f4ab7c8f15efdcd336fdf077ec683f4e7be
Luciano Daniel <42324730+lodaniel@users.noreply.github.com>
2022-08-09 07:08:16 -0300

    Add files via upload
    A morte do Peru

---
+++ peru.js
@@ -0,0 +1,17 @@
+/*
+Todo mundo fala que o peru morre de véspera
+A véspera do Natal é dia 24
+Ninguém morre na véspera, ele morre no dia
+O peru morre no Natal
+O peru precisa morrer antes do dia de Natal
+Algumas pessoas dão cachaça pro peru antes de morrer
+A antevéspera do Natal é 1 dia antes da morte do peru
+O peru não pode morrer na véspera de tomar cachaça
+O peru morre um dia depois da véspera
+Todo mundo sabe que ninguém morre depois da véspera
+Ninguém toma cachaça e morre antes do Natal
+A véspera do dia de tomar cachaça é o dia do peru
+Só o peru morre de véspera
+
+Pergunta: que dia que o peru morre?
+*/
